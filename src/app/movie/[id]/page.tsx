78da710731202e801cacf70803fb49f1c27a449b
onewayay <onewayay@naver.com>
2025-12-26 17:57:01 +0900

    day 13

--- src/app/movie/[id]/page.tsx
+++ src/app/movie/[id]/page.tsx
@@ -1,5 +1,31 @@
 import { MovieData } from '@/types';
 import style from './page.module.css';
+import { notFound } from 'next/navigation';
+
+export const dynamicParams = false;
+
+export function generateStaticParams() {
+  return [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+    { id: '4' },
+    { id: '5' },
+    { id: '6' },
+    { id: '7' },
+    { id: '8' },
+    { id: '9' },
+    { id: '10' },
+    { id: '11' },
+    { id: '12' },
+    { id: '13' },
+    { id: '14' },
+    { id: '15' },
+    { id: '16' },
+    { id: '17' },
+    { id: '18' },
+  ];
+}
 
 export default async function Page({
   params,
@@ -14,7 +40,10 @@ export default async function Page({
   );
 
   if (!response.ok) {
-    return <div>오류가 발생했습니다.</div>;
+    if (response.status === 404) {
+      notFound();
+    }
+    return <div>오류가 발생했습니다...</div>;
   }
 
   const movie: MovieData = await response.json();
